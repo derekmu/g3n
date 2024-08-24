86c7f6278337e99e1e36d20a7e2de8d12dc80270
Derek Mulvihill <mulvihd@gmail.com>
2024-08-23 21:16:08 -0500

    tweak: bring back ShaderDefines.Equals to check performance

--- renderer/shaman.go
+++ renderer/shaman.go
@@ -248,7 +248,7 @@ func (ss *ShaderSpecs) equals(other *ShaderSpecs) bool {
 		ss.PointLightsMax == other.PointLightsMax &&
 		ss.SpotLightsMax == other.SpotLightsMax &&
 		ss.MatTexturesMax == other.MatTexturesMax &&
-		maps.Equal(ss.MatDefines, other.MatDefines) &&
-		maps.Equal(ss.GeomDefines, other.GeomDefines) &&
-		maps.Equal(ss.GrDefines, other.GrDefines)
+		ss.MatDefines.Equals(other.MatDefines) &&
+		ss.GeomDefines.Equals(other.GeomDefines) &&
+		ss.GrDefines.Equals(other.GrDefines)
 }
